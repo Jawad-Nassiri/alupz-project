b9a2df823dac0f1de612d3498cb39295a165abdb
Jawad <jawadnasiri2222@gmail.com>
2025-10-12 16:00:29 +0200

    add hero section with image and heading layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
-import headerBanner from "../../images/header-banner.jpg";
+import headerBanner from "../../images/header/header-banner.jpg";
+import headerSubBanner from "../../images/header/header-subbanner.jpg";
 import "./Header.css";
 import Nav from "./Nav";
 
 export default function Header() {
   return (
     <div className="header" style={{ backgroundImage: `url(${headerBanner})` }}>
+
       <Nav />
+      <div className="hero">
+        <h1 className="title">Come to</h1>
+
+        <div className="subtitle">
+          <img src={headerSubBanner} alt="AI" />
+          <h1>
+            The world of <span>AI</span>
+          </h1>
+        </div>
+      </div>
+
     </div>
   );
 }
