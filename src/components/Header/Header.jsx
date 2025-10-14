ba93c9b01858f447e2f2a37e846462b90967eac6
Jawad <jawadnasiri2222@gmail.com>
2025-10-14 18:31:42 +0200

    add the responsive design to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import headerBanner from "../../images/header/header-banner.jpg";
 import "./Header.css";
 import Nav from "./Nav/Nav";
 import Hero from "./Hero/Hero";
+import { FaBars } from "react-icons/fa";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="header" style={{ backgroundImage: `url(${headerBanner})` }}>
-      <Nav />
+      <FaBars className="header__menu-icon" onClick={() => setIsOpen(true)} />
+      <Nav isOpen={isOpen} setIsOpen={setIsOpen} />
       <Hero />
     </div>
   );
--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,32 +1,63 @@
 import "./Nav.css";
-import logo from '../../../images/white-logo.png';
-import { FaSearch } from 'react-icons/fa';
+import logo from "../../../images/white-logo.png";
+import { FaSearch, FaTimes } from "react-icons/fa";
 
-export default function Nav() {
+export default function Nav({isOpen, setIsOpen }) {
   return (
-    <div className="container">
-          <div className="nav">
-              <div className="logo">
-                  <img src={logo} alt="logo" />
-              </div>
-              <div className="list">
-                  <ul className="nav-list">
-                      <li className="nav-item"><a href="#" className="nav-link active">Home</a></li>
-                      <li className="nav-item"><a href="#" className="nav-link">About</a></li>
-                      <li className="nav-item"><a href="#" className="nav-link">Services</a></li>
-                      <li className="nav-item"><a href="#" className="nav-link">Pages</a></li>
-                      <li className="nav-item"><a href="#" className="nav-link">Blog</a></li>
-                      <li className="nav-item"><a href="#" className="nav-link">Content</a></li>
-                  </ul>
-              </div>
-              <div className="search">
-                  <div className="search-icon">
-                      <a href="#" className="search-icon__link"><FaSearch/></a>
-                  </div>
-                  <div className="see-more">
-                      <a href="#" className="see-more--link">See More</a>
-                  </div>
-              </div>
+    <div className={`container ${isOpen ? "active" : ""}`}>
+      <div className="nav">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="list">
+          <ul className="nav-list">
+            <li className="nav-item__x-icon" onClick={() => setIsOpen(false)}>
+              <FaTimes />
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link active">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Pages
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Content
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="search">
+          <div className="search-icon">
+            <a href="#" className="search-icon__link">
+              <FaSearch />
+            </a>
+          </div>
+          <div className="see-more">
+            <a href="#" className="see-more--link">
+              See More
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
