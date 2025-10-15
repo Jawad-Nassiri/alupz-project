39a7d421aed2b6e617c2fa32763600d48446c49e
Jawad <jawadnasiri2222@gmail.com>
2025-10-15 09:26:56 +0200

    add responsive design to the approach part

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ export default function App() {
       <Header />
       <Stats />
       <AiServices />
-      {/* <Approach />
-      <Introduction />
+      <Approach />
+      {/* <Introduction />
       <Testimonials />
       <Team />
       <ContactUs />
