58a992e1dc36dc2b560366f55b829c4ecb2c5f83
Jawad <jawadnasiri2222@gmail.com>
2025-10-15 10:28:02 +0200

    add responsive design ot the introduction part

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ export default function App() {
       <Stats />
       <AiServices />
       <Approach />
-      {/* <Introduction />
-      <Testimonials />
+      <Introduction />
+      {/* <Testimonials />
       <Team />
       <ContactUs />
       <Footer/> */}
